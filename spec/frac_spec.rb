9b25744f1d8318b6b8346dd7fa4c53578955a903
alu0100596113 <alu0100596113@ull.edu.es>
2013-10-23 16:05:13 +0100

    Se crea una espectativa para almacenar y acceder al numerador y denominador

--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -1,5 +1,12 @@
 require "lib/frac.rb"
 
-describe Frac do 
-
+describe Frac do
+	describe "# almacenamiento del numerador y denominador." do
+		it "se almacena correctamente el numerador." do
+			@f1.numerador.should eq(1)
+		end
+		it "se almacena correctamente el denominador" do
+			@f1.denominador.should eq(1)		
+		end
+	end
 end 
