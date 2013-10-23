45864cbb79eade2de81f2a97a870cbe5c94040aa
EliezerCruzSuarez <alu0100611298@ull.edu.es>
2013-10-23 18:32:31 +0100

    Incluida expectatica para mostrar fracción en forma flotante.

--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -4,6 +4,7 @@ describe Frac do
 	before	:each do
 		@f1 = Frac.new(1,1)
 		@f2 = Frac.new(2,2)
+		@f3 = Frac.new(1,2)
 	end
 	describe "# almacenamiento del numerador y denominador." do
 		it "se almacena correctamente el numerador." do
@@ -32,6 +33,11 @@ describe Frac do
 	describe "# se debe imprimir" do
 		it "numerador/denominador" do
 			@f1.to_s.should eq("1/1")
+		end
+	end
+	describe "# se debe imprimir" do
+		it " la fraccion en coma flotante" do
+			@f3.to_f.should eq(0.5)
 		end
 	end
 end
