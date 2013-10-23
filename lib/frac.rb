a50068decd848fe4ed0586f22e3aefd4cd200a08
alu0100596113 <alu0100596113@ull.edu.es>
2013-10-23 16:11:27 +0100

    Cumplida la espectativa de almacenamiento de numerador y denominador

--- lib/frac.rb
+++ lib/frac.rb
@@ -1,2 +1,6 @@
 class Frac
+	attr_reader :numerador, :denominador
+	def initialize(numerador, denominador)
+		@numerador , @denominador = numerador, denominador
+	end
 end
--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -1,6 +1,9 @@
 require "lib/frac.rb"
 
 describe Frac do
+	before	:each do
+		@f1 = Frac.new(1,1)
+	end
 	describe "# almacenamiento del numerador y denominador." do
 		it "se almacena correctamente el numerador." do
 			@f1.numerador.should eq(1)
