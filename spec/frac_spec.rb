d5a4479686f98dd94289fc3571f11f9b15d90c02
alu0100596113 <alu0100596113@ull.edu.es>
2013-10-23 16:31:43 +0100

    Se incluye la espectativa para comprobar que una fracción está en su forma reducida.

--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -3,6 +3,7 @@ require "lib/frac.rb"
 describe Frac do
 	before	:each do
 		@f1 = Frac.new(1,1)
+		@f2 = Frac.new(2,2)
 	end
 	describe "# almacenamiento del numerador y denominador." do
 		it "se almacena correctamente el numerador." do
@@ -12,4 +13,12 @@ describe Frac do
 			@f1.denominador.should eq(1)		
 		end
 	end
-end 
+	describe "# debe estar en su forma reducida" do
+		it "Se reducen el numerador" do
+			@f2.numerador.should eq(1)
+		end
+		it "Se reduce el denominador" do
+			@f2.denominador.should eq(1)
+		end
+	end
+end
