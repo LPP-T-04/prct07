6e77ea7091f62a68e80a8437af1cfdb2eabbb293
alu0100596113 <alu0100596113@ull.edu.es>
2013-10-24 13:34:42 +0100

    Se cumple la espectativa para la suma de dos fracciones.

--- lib/frac.rb
+++ lib/frac.rb
@@ -3,25 +3,32 @@ require "./lib/gcd.rb"
 class Frac
 	attr_reader :numerador, :denominador
 	include Comparable
-	def initialize(numerador, denominador)
+	
+    def initialize(numerador, denominador)
 		mcd = gcd(numerador,denominador)
 		@numerador , @denominador = numerador/mcd, denominador/mcd
 	end
-	def num()
+	
+    def num()
 		@numerador
 	end
-	def denom()
+	
+    def denom()
 		@denominador
 	end
-	def to_s()
+	
+    def to_s()
 		"#{@numerador}/#{@denominador}"
 	end
-	def to_f()
+	
+    def to_f()
 		@numerador.to_f/@denominador
 	end
-	def <=>(other)
+	
+    def <=>(other)
     	@numerador.to_f/@denominador <=> other.numerador.to_f/other.denominador
     end
+    
     def abs()
     	if (@numerador > 0 && @denominador > 0)
     		"#{@numerador}/#{@denominador}"
@@ -33,9 +40,11 @@ class Frac
     		"#{-1 * @numerador}/#{-1 * @denominador}"
     	end  						
     end
+    
     def reciprocal()
     	Frac.new(@denominador, @numerador)
     end
+
     def -()
     	if (@numerador > 0 && @denominador > 0)
     		Frac.new(-1 * @numerador, @denominador)
@@ -46,6 +55,10 @@ class Frac
     	else (@numerador < 0 && @denominador < 0)
     		Frac.new(@numerador, -1 * @denominador)
     	end
+    end
+
+    def +(other)
+        Frac.new(@numerador*other.denominador + other.numerador*@denominador , @denominador*other.denominador)
     end
 end
 
